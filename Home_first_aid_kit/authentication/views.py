ae9f181a68a9d1a91c084a7b4f2a62a50b2e5e8e
Aleksandr <a.shaddar@mail.ru>
2024-06-10 21:21:33 +0300

    Save working process (added login, logout, register)

--- Home_first_aid_kit/authentication/views.py
+++ Home_first_aid_kit/authentication/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from authentication.models import User
 from django.urls import reverse
 
 from .forms import AuthenticationForm, RegisterUserForm
@@ -21,11 +20,6 @@ def login_user(request):
             if user:
                 login(request, user)
                 return HttpResponseRedirect(reverse('main'))
-
-
-            # user = User.objects.filter(name=name).first()
-            # if user is not None and user.password == password:
-            #     return HttpResponseRedirect('medicaments/')
             else:
                 message = 'Пользователя с такими данными не существует. Попробуйте еще раз!'
                 return render(request, 'authentication/login.html', {'message': message, 'form': form})
@@ -36,7 +30,7 @@ def login_user(request):
 
 def logout_user(request):
     logout(request)
-    return HttpResponseRedirect('')
+    return HttpResponseRedirect(reverse('login_user'))
 
 
 def register(request):
@@ -46,7 +40,9 @@ def register(request):
             user = form.save(commit=False)
             user.set_password(form.cleaned_data['password'])
             user.save()
-            return render(request, 'authentication/login.html')
+            form = AuthenticationForm()
+            # return HttpResponseRedirect(reverse('login_user'))
+            return render(request, 'authentication/register_done.html')
     else:
         form = RegisterUserForm()
     return render(request, 'authentication/register.html', {'form': form})
